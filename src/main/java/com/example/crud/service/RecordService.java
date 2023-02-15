5479915c055b534e5be2a19eec120e3c44737395
Yaroslav Tsebro <yaroslav.cebro@gmail.com>
2023-02-15 17:45:33 +0200

    add service

--- src/main/java/com/example/crud/service/RecordService.java
+++ src/main/java/com/example/crud/service/RecordService.java
@@ -1,12 +1,17 @@
 package com.example.crud.service;
 
+import com.example.crud.dto.CreateRecordDTO;
+import com.example.crud.dto.UpdateRecordDTO;
 import com.example.crud.model.Record;
 
 import java.util.Collection;
+import java.util.Optional;
 
 public interface RecordService {
     Collection<Record> findAll();
     Record findByTitle(String title);
     Boolean deleteById(Long id);
-    Record updateById(Record record);
+    Record updateById(UpdateRecordDTO dto);
+    Record insertRecord(CreateRecordDTO dto);
+    Optional<Record> findById(Long id);
 }
--- src/main/java/com/example/crud/service/implementation/RecordServiceImp.java
+++ src/main/java/com/example/crud/service/implementation/RecordServiceImp.java
@@ -1,5 +1,9 @@
 package com.example.crud.service.implementation;
 
+import com.example.crud.dto.CreateRecordDTO;
+import com.example.crud.dto.UpdateRecordDTO;
+import com.example.crud.exception.ResourceNotFoundException;
+import com.example.crud.mapper.RecordMapper;
 import com.example.crud.model.Record;
 import com.example.crud.repo.RecordRepo;
 import com.example.crud.service.RecordService;
@@ -9,6 +13,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.Collection;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Transactional
@@ -16,6 +21,7 @@ import java.util.Collection;
 @Service
 public class RecordServiceImp implements RecordService {
     private final RecordRepo recordRepo;
+    private final RecordMapper recordMapper;
 
     @Override
     public Collection<Record> findAll() {
@@ -37,9 +43,29 @@ public class RecordServiceImp implements RecordService {
     }
 
     @Override
-    public Record updateById(Record record) {
-        log.info("Update by id: "+ record.getId());
-        log.info("Update info: "+ record);
+    public Record updateById(UpdateRecordDTO dto) {
+        Record recordToCreate = recordMapper.toUpdateRecord(dto);
+        Long id = recordToCreate.getId();
+        Record record = recordRepo.findById(recordToCreate.getId())
+                .orElseThrow(() -> new ResourceNotFoundException("Employee not exist with id: " + recordToCreate.getId()));
+
+        record.setTitle(recordToCreate.getTitle());
+        record.setText(recordToCreate.getText());
+        log.info("Update by id: "+ recordToCreate.getId());
+        log.info("Update info: "+ recordToCreate);
         return recordRepo.save(record);
+    }
+
+    @Override
+    public Record insertRecord(CreateRecordDTO dto) {
+        Record recordToCreate = recordMapper.toCreateRecord(dto);
+        log.info("insert info: "+ recordToCreate);
+        return recordRepo.save(recordToCreate);
+    }
+
+    @Override
+    public Optional<Record> findById(Long id) {
+        log.info("Find by id: "+ id);
+        return recordRepo.findById(id);
     }
 }
